f34cfd3cac2982baed09f7cfccd6200a5216e5b6
Manish ubnare <manishub75@gmail.com>
2025-08-24 16:25:02 +0530

    feat(auth): Add complete login component functionality

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,15 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/Logoutbtn";
+import Button from "./Button"
+import Input from "./Input"
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input
 }
